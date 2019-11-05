6a2c438c21bcedb48649703cb7ee8544044b9b53
Student <example@example.com>
2019-11-05 14:21:01 -0800

    added ticket reciept and assign id

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
+//Business Logic
+function TicketRecipt(){
+  this.tickets = [],
+  this.currentId = 0
+}
+
+TicketRecipt.prototype.addTicket = function(ticket) {
+  ticket.id = this.assignId();
+  this.tickets.push(ticket);
+}
+
+TicketRecipt.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+
 function Ticket (title, time, finalPrice){
   this.title = title,
   this.time = time,
   this.price = price
-
 }
-
 
 var ticketPrice = function(movieTime, seniorDiscount, premium){
   if (seniorDiscount === 'on') {
@@ -15,50 +30,33 @@ var ticketPrice = function(movieTime, seniorDiscount, premium){
     price = 12;
   } else {
     price = 10}
-  console.log(price);
   return price;
 }
 
+function displayTicket(ticketToDisplay){
+  var ticketResult = $('.ticketResult');
+}
 
-
-
-
+//User Logic
+var ticketRecipt = new TicketRecipt();
 
 $(document).ready(function () {
   $('#movieTickets').submit(function (event) {
     event.preventDefault();
-
-
     var movieTitle = $("#movieTitles").val();
     var movieTime = $("#movieTimes").val();
     var seniorDiscount =$("#seniorDiscount:checked").val();
     var premium;
-
     if (movieTitle === "PremiumMovie"){
       premium = true
     }
+
+
     var finalPrice = ticketPrice(movieTime, seniorDiscount, premium)
+    let ticket = new Ticket(movieTitle, movieTime, finalPrice)
 
+    ticketRecipt.addTicket(ticket);
 
-
-
-
-
-
-
-
-    let myTicket = new Ticket(movieTitle, movieTime, finalPrice)
-    console.log(myTicket);
-    // myTicket.price();
-
-
-    // console.log(myTicket);
-    // console.log(myTicket.discount);
-
-
-
-
-
-
+    console.log(ticketRecipt);
   })
 });
