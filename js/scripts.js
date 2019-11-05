d2c6c764b4a2ca5a29df02305f876773f804f759
Student <example@example.com>
2019-11-05 14:35:18 -0800

    display object on DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,18 @@
 //Business Logic
-function TicketRecipt(){
+function TicketReciept(){
   this.tickets = [],
   this.currentId = 0
 }
 
-TicketRecipt.prototype.addTicket = function(ticket) {
+TicketReciept.prototype.addTicket = function(ticket) {
   ticket.id = this.assignId();
   this.tickets.push(ticket);
 }
 
-TicketRecipt.prototype.assignId = function() {
+TicketReciept.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
 }
-
 
 function Ticket (title, time, finalPrice){
   this.title = title,
@@ -23,22 +22,27 @@ function Ticket (title, time, finalPrice){
 
 var ticketPrice = function(movieTime, seniorDiscount, premium){
   if (seniorDiscount === 'on') {
-    price = 7;
+    price = '$' + 7 + ' - Senior Discount';
   } else if(movieTime === '3pm') {
-    price = 9;
+    price = '$' + 9 + ' - Matinee Discount';
   } else if(premium === true){
-    price = 12;
+    price = '$' + 12 + ' - First Run';
   } else {
-    price = 10}
+    price = '$' + 10}
   return price;
 }
 
 function displayTicket(ticketToDisplay){
-  var ticketResult = $('.ticketResult');
+  var ticketList = $("ul#userTicket");
+  var htmlForContactInfo = "";
+  ticketToDisplay.tickets.forEach(function(ticket) {
+    htmlForContactInfo += "<li id=" + ticket.id + ">" + "Title: " + ticket.title + " <br>Time: " + ticket.time + "<br> Price: " + ticket.price +  "</li><br>";
+  });
+  ticketList.html(htmlForContactInfo);
 }
 
 //User Logic
-var ticketRecipt = new TicketRecipt();
+var ticketReciept = new TicketReciept();
 
 $(document).ready(function () {
   $('#movieTickets').submit(function (event) {
@@ -51,12 +55,12 @@ $(document).ready(function () {
       premium = true
     }
 
-
     var finalPrice = ticketPrice(movieTime, seniorDiscount, premium)
     let ticket = new Ticket(movieTitle, movieTime, finalPrice)
 
-    ticketRecipt.addTicket(ticket);
+    ticketReciept.addTicket(ticket);
 
-    console.log(ticketRecipt);
+    console.log(ticketReciept);
+    displayTicket(ticketReciept)
   })
 });
